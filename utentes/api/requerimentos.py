fcbc4fcd2b72322898aa087219527eee4030eaac
Francisco Puga <fpuga@icarto.es>
2019-01-28 19:45:26 +0100

    Formatting

--- utentes/api/requerimentos.py
+++ utentes/api/requerimentos.py
@@ -1,22 +1,32 @@
 # -*- coding: utf-8 -*-
 
+import datetime
+import logging
+
 from pyramid.view import view_config
+
+from sqlalchemy.orm.exc import MultipleResultsFound, NoResultFound
+from utentes.api.error_msgs import error_msgs
+from utentes.models.ara import Ara
 from utentes.models.base import badrequest_exception
 from utentes.models.exploracao import Exploracao
-from utentes.models.ara import Ara
-from utentes.api.error_msgs import error_msgs
-from sqlalchemy.orm.exc import MultipleResultsFound, NoResultFound
+from utentes.user_utils import (
+    PERM_CREATE_REQUERIMENTO, PERM_GET, PERM_UPDATE_REQUERIMENTO,
+)
 
-from utentes.user_utils import PERM_GET, PERM_UPDATE_REQUERIMENTO, PERM_CREATE_REQUERIMENTO
-
-import datetime
-
-import logging
 log = logging.getLogger(__name__)
 
 
-@view_config(route_name='api_requerimento', permission=PERM_GET, request_method='GET', renderer='json')
-@view_config(route_name='api_requerimento_id', permission=PERM_GET, request_method='GET', renderer='json')
+@view_config(
+    route_name='api_requerimento',
+    permission=PERM_GET,
+    request_method='GET',
+    renderer='json')
+@view_config(
+    route_name='api_requerimento_id',
+    permission=PERM_GET,
+    request_method='GET',
+    renderer='json')
 def requerimento_get(request):
     gid = None
     if request.matchdict:
@@ -24,8 +34,9 @@ def requerimento_get(request):
 
     if gid:  # return individual explotacao
         try:
-            return request.db.query(Exploracao).filter(Exploracao.gid == gid).one()
-        except(MultipleResultsFound, NoResultFound):
+            return request.db.query(Exploracao).filter(
+                Exploracao.gid == gid).one()
+        except (MultipleResultsFound, NoResultFound):
             raise badrequest_exception({
                 'error': error_msgs['no_gid'],
                 'gid': gid
@@ -34,18 +45,24 @@ def requerimento_get(request):
     else:  # return collection
         states = request.GET.getall('states[]')
         if states:
-            features = request.db.query(Exploracao).filter(Exploracao.estado_lic.in_(states)).all()
+            features = request.db.query(Exploracao).filter(
+                Exploracao.estado_lic.in_(states)).all()
         else:
             features = request.db.query(Exploracao).all()
 
-        return {
-            'type': 'FeatureCollection',
-            'features': features
-        }
+        return {'type': 'FeatureCollection', 'features': features}
 
 
-@view_config(route_name='api_requerimento_id', permission=PERM_UPDATE_REQUERIMENTO, request_method='PATCH', renderer='json')
-@view_config(route_name='api_requerimento_id', permission=PERM_UPDATE_REQUERIMENTO, request_method='PUT', renderer='json')
+@view_config(
+    route_name='api_requerimento_id',
+    permission=PERM_UPDATE_REQUERIMENTO,
+    request_method='PATCH',
+    renderer='json')
+@view_config(
+    route_name='api_requerimento_id',
+    permission=PERM_UPDATE_REQUERIMENTO,
+    request_method='PUT',
+    renderer='json')
 def requerimento_update(request):
     gid = request.matchdict['id']
     body = request.json_body
@@ -56,7 +73,11 @@ def requerimento_update(request):
     return e
 
 
-@view_config(route_name='api_requerimento', permission=PERM_CREATE_REQUERIMENTO, request_method='POST', renderer='json')
+@view_config(
+    route_name='api_requerimento',
+    permission=PERM_CREATE_REQUERIMENTO,
+    request_method='POST',
+    renderer='json')
 # admin || administrativo
 def requerimento_create(request):
     try:
@@ -90,8 +111,12 @@ def calculate_new_exp_id(request, ara):
     return '{}/{}/{}'.format(next_id, ara, year)
 
 
-@view_config(route_name='api_requerimento_get_datos_ara', permission=PERM_GET, request_method='GET', renderer='json')
+@view_config(
+    route_name='api_requerimento_get_datos_ara',
+    permission=PERM_GET,
+    request_method='GET',
+    renderer='json')
 def get_datos_ara(request):
-    ara = request.registry.settings.get('ara');
+    ara = request.registry.settings.get('ara')
     result = request.db.query(Ara).filter(Ara.id == ara).one()
-    return result;
+    return result
