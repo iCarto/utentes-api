da45441eede99a4bea34db5f9d4d613d0bf7c153
Francisco Puga <fpuga@icarto.es>
2019-01-28 17:27:38 +0100

    Fixes bug when printing invoices
    The new code does not produce an exception when the are not previous invoices
    in the database, and also begins the invoice number in 1. (0 is for techies)
    
    ```
    params['next_serial'] = (request.db.execute(sql).first() or [
            FACTURAS_DEFAULT_VALUES.get('{unidad}/{ano}'.format(**params), 1)
        ])[0]
    ```
    
    Also, the number is formated with '0'
    
    ```
    num_factura = '{next_serial:04d}-{unidad}/{ano}'.format(**params)
    ```
    
    refs #1538, refs #1393

--- utentes/api/facturacao.py
+++ utentes/api/facturacao.py
@@ -1,19 +1,29 @@
 # -*- coding: utf-8 -*-
 
+import logging
+
 from pyramid.view import view_config
+
+from sqlalchemy.orm.exc import MultipleResultsFound, NoResultFound
+from utentes.api.error_msgs import error_msgs
 from utentes.models.base import badrequest_exception
 from utentes.models.exploracao import Exploracao
 from utentes.models.facturacao import Facturacao
-from utentes.api.error_msgs import error_msgs
-from sqlalchemy.orm.exc import MultipleResultsFound, NoResultFound
-from utentes.user_utils import PERM_GET, PERM_FACTURACAO
+from utentes.user_utils import PERM_FACTURACAO, PERM_GET
 
-import logging
 log = logging.getLogger(__name__)
 
 
-@view_config(route_name='api_facturacao', permission=PERM_GET, request_method='GET', renderer='json')
-@view_config(route_name='api_facturacao_id', permission=PERM_GET, request_method='GET', renderer='json')
+@view_config(
+    route_name='api_facturacao',
+    permission=PERM_GET,
+    request_method='GET',
+    renderer='json')
+@view_config(
+    route_name='api_facturacao_id',
+    permission=PERM_GET,
+    request_method='GET',
+    renderer='json')
 def facturacao_get(request):
     gid = None
     if request.matchdict:
@@ -21,8 +31,9 @@ def facturacao_get(request):
 
     if gid:  # return individual explotacao
         try:
-            return request.db.query(Exploracao).filter(Exploracao.gid == gid).one()
-        except(MultipleResultsFound, NoResultFound):
+            return request.db.query(Exploracao).filter(
+                Exploracao.gid == gid).one()
+        except (MultipleResultsFound, NoResultFound):
             raise badrequest_exception({
                 'error': error_msgs['no_gid'],
                 'gid': gid
@@ -40,13 +51,14 @@ def facturacao_get(request):
             query = query.filter(Exploracao.fact_estado.in_(fact_estado))
 
         features = query.all()
-        return {
-            'type': 'FeatureCollection',
-            'features': features
-        }
+        return {'type': 'FeatureCollection', 'features': features}
 
 
-@view_config(route_name='api_facturacao_new_factura', permission=PERM_FACTURACAO, request_method='GET', renderer='json')
+@view_config(
+    route_name='api_facturacao_new_factura',
+    permission=PERM_FACTURACAO,
+    request_method='GET',
+    renderer='json')
 def num_factura_get(request):
 
     gid = None
@@ -54,35 +66,30 @@ def num_factura_get(request):
         gid = request.matchdict['id'] or None
 
     if not gid:
-        raise badrequest_exception({
-            'error': error_msgs['no_gid'],
-            'gid': gid
-        })
+        raise badrequest_exception({'error': error_msgs['no_gid'], 'gid': gid})
 
     try:
-        facturacao = request.db.query(Facturacao).filter(Facturacao.gid == gid).one()
-    except(MultipleResultsFound, NoResultFound):
-        raise badrequest_exception({
-            'error': error_msgs['no_gid'],
-            'gid': gid
-        })
+        facturacao = request.db.query(Facturacao).filter(
+            Facturacao.gid == gid).one()
+    except (MultipleResultsFound, NoResultFound):
+        raise badrequest_exception({'error': error_msgs['no_gid'], 'gid': gid})
 
     exp_id = facturacao.exploracao
     if facturacao.fact_id is not None:
         return facturacao.fact_id
 
     try:
-        exploracao = request.db.query(Exploracao).filter(Exploracao.gid == exp_id).one()
-    except(MultipleResultsFound, NoResultFound):
-        raise badrequest_exception({
-            'error': error_msgs['no_gid'],
-            'gid': gid
-        })
+        exploracao = request.db.query(Exploracao).filter(
+            Exploracao.gid == exp_id).one()
+    except (MultipleResultsFound, NoResultFound):
+        raise badrequest_exception({'error': error_msgs['no_gid'], 'gid': gid})
 
     if not exploracao.loc_unidad:
         raise badrequest_exception({
-            'error': 'A unidade é un campo obligatorio',
-            'exp_id': exp_id
+            'error':
+            'A unidade é un campo obligatorio',
+            'exp_id':
+            exp_id
         })
 
     params = {
@@ -107,12 +114,11 @@ def num_factura_get(request):
         'UGBS/2018': [1500]
     }
 
-    params['next_serial'] = (
-        request.db.execute(sql).first()
-        or FACTURAS_DEFAULT_VALUES.get('{unidad}/{ano}'.format(**params), 0)
-    )[0]
+    params['next_serial'] = (request.db.execute(sql).first() or [
+        FACTURAS_DEFAULT_VALUES.get('{unidad}/{ano}'.format(**params), 1)
+    ])[0]
 
-    num_factura = '{next_serial:4d}-{unidad}/{ano}'.format(**params)
+    num_factura = '{next_serial:04d}-{unidad}/{ano}'.format(**params)
 
     facturacao.fact_id = num_factura
 
@@ -122,8 +128,16 @@ def num_factura_get(request):
     return num_factura
 
 
-@view_config(route_name='api_facturacao_id', permission=PERM_FACTURACAO, request_method='PATCH', renderer='json')
-@view_config(route_name='api_facturacao_id', permission=PERM_FACTURACAO, request_method='PUT', renderer='json')
+@view_config(
+    route_name='api_facturacao_id',
+    permission=PERM_FACTURACAO,
+    request_method='PATCH',
+    renderer='json')
+@view_config(
+    route_name='api_facturacao_id',
+    permission=PERM_FACTURACAO,
+    request_method='PUT',
+    renderer='json')
 def facturacao_update(request):
     id = request.matchdict['id']
     body = request.json_body
