2bdf7ebf654a596ff8d2895ca2988776cad802b6
Luis Sevillano <lsevillano@icarto.es>
2019-02-28 11:17:31 +0100

    Add the date type fields that were missing in Renovacao.js

--- utentes/static/models/Renovacao.js
+++ utentes/static/models/Renovacao.js
@@ -1,6 +1,16 @@
 Backbone.SIXHIARA = Backbone.SIXHIARA || {};
 Backbone.SIXHIARA.Renovacao = Backbone.Model.extend({
-    dateFields: ['d_soli', 'd_ultima_entrega_doc', 'd_emissao_sub_old', 'd_emissao_sub', 'd_emissao_sup_old', 'd_emissao_sup'],
+    dateFields: [
+        'd_soli',
+        'd_ultima_entrega_doc',
+        'd_emissao_sub_old',
+        'd_emissao_sub',
+        'd_emissao_sup_old',
+        'd_emissao_sup',
+        'd_validade_sub_old',
+        'd_validade_sub',
+        'd_validade_sup_old',
+        'd_validade_sup'],
 
     urlRoot: Backbone.SIXHIARA.Config.apiRenovacoes,
 
