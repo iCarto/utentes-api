910c53caf5bacbdf2e6d2bb9aa8c95eb93f5b175
Francisco Puga <fpuga@icarto.es>
2019-04-10 11:43:14 +0200

    Rol constant refactor. refs #1582. missed files

---
+++ users/user_roles.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+ROL_ADMIN = u'Administrador'
+ROL_ADMINISTRATIVO = u'Departamento Administrativo'  # DA
+ROL_FINANCIERO = u'Departamento Financeiro'  # DF
+ROL_DIRECCION = u'Direcção'
+ROL_TECNICO = u'Departamento Técnico'
+ROL_JURIDICO = u'Departamento Jurídico'  # DJ
+ROL_OBSERVADOR = u'Observador'
+ROL_UNIDAD_DELEGACION = u'Unidade ou Delegação'
+
+SINGLE_USER = 'SINGLE_USER'
+
