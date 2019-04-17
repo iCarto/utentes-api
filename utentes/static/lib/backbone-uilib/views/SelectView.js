ae8e476628e739ce6e40a2b21300c1b4e7a46d60
Francisco Puga <fpuga@icarto.es>
2019-04-17 09:36:57 +0200

    New SelectView Features: empty options and clone collections
    This commit introduces a lot of complexety in SelectView with just two new features:
    
    * Allows (it's the default now) to automatically create an empty `option` in the
      `select`. This should avoid all the awfull `unshift` all over the place and
      the difficult to handle `null` rows in the database for each parent group
    
    * To posibilly avoid troubles with listeners, a new feature to clone the
      collection is introduced. This is introducing complexity and probably is no
      need. It *must* be revisited in the future getting real use cases
    
    refs #1655

--- utentes/static/lib/backbone-uilib/views/SelectView.js
+++ utentes/static/lib/backbone-uilib/views/SelectView.js
@@ -1,13 +1,65 @@
 Backbone.UILib = Backbone.UILib || {};
 Backbone.UILib.SelectView = Backbone.View.extend({
 
-    initialize: function(){
+    initialize: function(options){
+        var defaultOptions = {
+            /* Adds an `option` like
+            {
+                'category': '<category>',
+                'text': null,
+                'order': 0,
+            }
+            to the `select` when there is no one in the collection. If the
+            collection is empty it does not append the empty option.
+            */
+            addVoidValue: true,
+            /*
+            Introducido después de mucho código hecho. Probablmente debería ser
+            opción por defecto y refactorizar el resto del código. De modo que
+            incluso en lugar de una collection se le pudiera pasar un `category`
+            y se hiciera el filtro aquí dentro
+            */
+            cloneCollection: false,
+        };
+        this.options = Object.assign({}, defaultOptions, options);
         this._subviews = [];
+        this._updateCollection(this.collection)
+    },
+
+    _updateCollection: function(newCollection) {
+        if (this.options.cloneCollection) {
+            this.collection = new Backbone.UILib.DomainCollection(newCollection.models);
+        } else {
+            this.collection = newCollection;
+        }
     },
 
     render: function(){
         var subviews = [];
         var content = document.createDocumentFragment();
+        if (this.collection.length === 0) {
+            this._updateDOMAndNotify(content, subviews);
+            return this;
+        }
+
+        if (this.isVoidNeed()) {
+            var emptyModel = new  Backbone.UILib.Domain({
+                'category': this.collection.at(0).get('category'),
+                'text': null,
+                'order': 0,
+            });
+            if (this.cloneCollection) {
+                this.collection.unshift(emptyModel);
+            } else {
+                var option = new Backbone.UILib.OptionView({
+                    model: emptyModel,
+                    text:  'text',
+                    attributes: null,
+                });
+                content.appendChild(option.render().el);
+                subviews.push(option);
+            }
+        }
 
         this.collection.forEach(function(model){
             var alias = model.get('alias');
@@ -20,6 +72,12 @@ Backbone.UILib.SelectView = Backbone.View.extend({
             subviews.push(option);
         }, this);
 
+        this._updateDOMAndNotify(content, subviews);
+
+        return this;
+    },
+
+    _updateDOMAndNotify: function(content, subviews) {
         // Update DOM and _subviews array at once.
         // This would minimize reflows to only 1 instead of one per subview.
         if(this.collection.length === 0) {
@@ -36,14 +94,22 @@ Backbone.UILib.SelectView = Backbone.View.extend({
         // Some views, as Widgets.js, are listening to this event
         // to update the model.
         this.$el.trigger('change');
+    },
 
-        return this;
+    isVoidNeed: function() {
+        if (!this.options.addVoidValue) {
+            return false;
+        }
+        if (this.collection.filter((d) => d.get('text') === null).length > 0) {
+            return false;
+        }
+        return true;
     },
 
     // Free old collection to be garbage collected after subviews are removed,
     // as each one has a reference to a model in the collection.
     update: function(newCollection){
-        this.collection = newCollection;
+        this._updateCollection(newCollection);
         this.render();
     },
 
