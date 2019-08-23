3304c86e4b31345167f4796d2e4087117bb25a4b
Francisco Puga <fpuga@icarto.es>
2019-08-23 13:21:39 +0200

    Ignore vendor folder for lint and style

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
     rules: {
         indent: ["error", 4],
         "linebreak-style": ["error", "unix"],
-        quotes: ["error", "single", {avoidEscape: true, allowTemplateLiterals: true}],
+        quotes: ["error", "double", {avoidEscape: true, allowTemplateLiterals: true}],
         "quote-props": ["error", "consistent"],
         semi: ["error", "always"],
     },
