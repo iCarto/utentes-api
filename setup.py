744e82924fe7ec33a5c3eff663e65656340a77a1
Francisco Puga <fpuga@icarto.es>
2019-11-07 13:14:46 +0100

    New version: 191107

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ excludes = ["utentes.tests"]
 
 setup(
     name="utentes",
-    version="190729.1",
+    version="191107",
     description="utentes",
     author="iCarto",
     author_email="info@icarto.es",
