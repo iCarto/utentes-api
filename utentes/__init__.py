f078f156d0099462b179e0f2cd614fc945be4380
Elías Gago <egago@icarto.es>
2020-02-14 09:41:37 +0100

    Remove decode method from "media_root" setting parameter

--- utentes/__init__.py
+++ utentes/__init__.py
@@ -47,7 +47,7 @@ def decimal_adapter(obj, request):
 def main(global_config, **settings):
     if is_single_user_mode(settings):
         media_root = os.path.join(home_directory(), settings.get("ara"), "media")
-        media_root = media_root.decode(sys.getfilesystemencoding())
+        # media_root = media_root.decode(sys.getfilesystemencoding())
         settings["media_root"] = media_root
 
     engine = engine_from_config(settings, "sqlalchemy.")
