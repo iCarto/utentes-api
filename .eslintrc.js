831460d696996a66b5ad27009e5e69254609ef6b
Francisco Puga <fpuga@icarto.es>
2020-08-04 17:44:36 +0200

    DEV STUFF

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,31 @@
 module.exports = {
     env: {
         browser: true,
+        es6: true, // https://caniuse.com/#feat=es6
+        jquery: true,
     },
+    plugins: ["backbone"],
+    parserOptions: {
+        ecmaVersion: 6,
+        sourceType: "script",
+        ecmaFeatures: {
+            impliedStrict: true,
+            jsx: false,
+            globalReturn: false,
+        },
+    },
+    extends: ["eslint:recommended", "plugin:backbone/recommended"],
     rules: {
         indent: ["error", 4],
         "linebreak-style": ["error", "unix"],
         quotes: ["error", "double", {avoidEscape: true, allowTemplateLiterals: true}],
         "quote-props": ["error", "consistent"],
         semi: ["error", "always"],
+        "backbone/no-view-qualified-jquery": "error",
+    },
+    globals: {
+        L: "readonly",
+        SIRHA: "readonly",
+        bootbox: "readonly",
     },
 };
