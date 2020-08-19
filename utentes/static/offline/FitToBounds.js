1c0eb88f04f2bb7d70200d1df021bac9c30f184e
Francisco Puga <fpuga@icarto.es>
2020-08-19 21:31:26 +0200

    Makes the usage of FitToBound a bit easier to use
    but still a lot of work can be done with this utility
    
    refs #1584

--- utentes/static/offline/FitToBounds.js
+++ utentes/static/offline/FitToBounds.js
@@ -1,7 +1,14 @@
 var FitToBounds = {
-    fitToBounds: function(map, bounds, boundsPadding, maxZoom, minZoom) {
-        maxZoom = maxZoom || Number.MAX_SAFE_INTEGER;
-        minZoom = minZoom || Number.MIN_SAFE_INTEGER;
+    defaults: {
+        maxZoom: Number.MAX_SAFE_INTEGER,
+        minZoom: Number.MIN_SAFE_INTEGER,
+        boundPadding: 0,
+    },
+
+    fitToBounds: function(map, bounds, _boundsPadding, _maxZoom, _minZoom) {
+        let maxZoom = _maxZoom || map.getMaxZoom() || FitToBounds.defaults.maxZoom;
+        let minZoom = _minZoom || map.getMinZoom() || FitToBounds.defaults.minZoom;
+        let boundsPadding = _boundsPadding || FitToBounds.defaults.boundPadding;
         map.fitBounds(bounds.pad(boundsPadding));
         var zoom = map.getZoom();
         if (zoom > maxZoom) {
@@ -15,18 +22,32 @@ var FitToBounds = {
         return map.getBounds();
     },
 
-    fit: function(map, boundsPadding, maxZoom, minZoom, layers) {
-        var layers = Array.isArray(layers) ? layers : [layers];
-        var bounds = layers.reduce(function(totalBounds, layer) {
-            var layerBounds = layer && layer.getBounds();
-            return totalBounds.extend(layerBounds);
-        }, new L.LatLngBounds());
+    fitToLayers: function(map, _layers, boundsPadding, maxZoom, minZoom) {
+        let bounds;
+        if (_layers.getBounds) {
+            bounds = _layers.getBounds();
+        } else {
+            var layers = Array.isArray(_layers) ? _layers : [_layers];
+            bounds = layers.reduce(function(totalBounds, layer) {
+                if (layer && layer.getBounds) {
+                    var layerBounds = layer.getBounds();
+                    return totalBounds.extend(layerBounds);
+                }
+                return totalBounds.extend(layer.getLatLng());
+            }, new L.LatLngBounds());
+        }
 
         return FitToBounds.fitToBounds(map, bounds, boundsPadding, maxZoom, minZoom);
     },
 
-    fitAndSetMaxBounds: function(map, boundsPadding, maxZoom, minZoom, layers) {
-        let mapBounds = FitToBounds.fit(map, boundsPadding, maxZoom, minZoom, layers);
+    fitAndSetMaxBounds: function(map, layers, boundsPadding, maxZoom, minZoom) {
+        let mapBounds = FitToBounds.fitToLayers(
+            map,
+            layers,
+            boundsPadding,
+            maxZoom,
+            minZoom
+        );
         // el padding es para asegurarse de que entra
         // si no intenta hacer un _panInsideMaxBounds y puede
         // entrar en un bucle infinito
